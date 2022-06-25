0292535d6a99c02a2c3ec37ef05bc74422c8d438
Yaotian_Liu <2416224849@qq.com>
2022-06-25 21:16:44 +0800

    fix a bug in factor_predictor

---
+++ factorVAE/factor_predictor.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+from factorVAE.basic_net import MLP
+
+
+class FactorPredictor(nn.Module):
+    def __init__(self, latent_size, factor_size, stock_size):
+        super(FactorPredictor, self).__init__()
+
+        self.multi_head_attention = nn.MultiheadAttention(latent_size, factor_size)
+
+        self.distribution_network_mu = MLP(
+            input_size=stock_size * latent_size, output_size=factor_size, hidden_size=64
+        )
+
+        self.distribution_network_sigma = MLP(
+            input_size=stock_size * latent_size,
+            output_size=factor_size,
+            hidden_size=64,
+            out_activation=nn.Softplus(),
+        )
+
+    def forward(self, latent_features):
+        h = self.multi_head_attention(
+            latent_features, latent_features, latent_features
+        )[0].flatten()
+
+        mu_prior = self.distribution_network_mu(h)
+        sigma_prior = self.distribution_network_sigma(h)
+
+        return mu_prior, sigma_prior
